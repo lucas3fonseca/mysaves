f25fe8cd578623170f0eadee262da94a4e90c532
Lucas Fonseca <lfonseca@efuse.io>
2024-01-12 16:57:23 -0500

    enable cors

--- next.config.js
+++ next.config.js
@@ -15,6 +15,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://mysaves-c767g7wbf-lucasfonseca.vercel.app/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
