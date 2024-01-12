c806f9805d5e188c3efc2e528cd0d20e36acd1a2
Lucas Fonseca <lfonseca@efuse.io>
2024-01-12 17:19:37 -0500

    the react player might be crashing

--- src/mySave/components/MySaveView.tsx
+++ src/mySave/components/MySaveView.tsx
@@ -7,13 +7,14 @@ interface MySaveViewProps {
 }
 
 export const MySaveView = ({ mySave }: MySaveViewProps) => {
+  console.log(mySave)
   return (
-    <div className='flex flex-col justify-center align-middle ml-auto mr-auto w-1/2 pt-3'>
-      
-      <h1 className='text-xl font-semibold mb-3'>{mySave.title}</h1>
-      <h2>Description</h2>
-      <p className='text-l mb-5 mt-5'>{mySave.description}</p>
-      <ReactPlayer url={mySave.videoUrl} muted playing volume={1} playsinline controls />
-    </div>
+    <></>
+    // <div className='flex flex-col justify-center align-middle ml-auto mr-auto w-1/2 pt-3'>
+    //   <h1 className='text-xl font-semibold mb-3'>{mySave.title}</h1>
+    //   <h2>Description</h2>
+    //   <p className='text-l mb-5 mt-5'>{mySave.description}</p>
+    //   <ReactPlayer url={mySave.videoUrl} muted playing volume={1} playsinline controls />
+    // </div>
   )
 }
