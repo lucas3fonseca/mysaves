8b1ed5f3ebf8b8b0d17f01d4866f57435ed4d843
Lucas Fonseca <lfonseca@efuse.io>
2024-01-14 12:14:00 -0500

    update my saves query

--- pages/api/index.page.ts
+++ pages/api/index.page.ts
@@ -1,19 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-/*
- * Data stored in memory for this project, a re-deploy will wipe everything
- * This is pre-populated with some data for demo purposes.
- *
- * Need to hook into global namespace since the routes are bundled separately
- */
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<undefined>,
 ) {
-  // init state
-  if (!global.state) {
-    global.state = {}
-  }
-
   res.status(200).json(undefined)
 }
--- pages/api/my-saves/index.page.ts
+++ pages/api/my-saves/index.page.ts
@@ -3,13 +3,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { AppState, HttpRequestMethods } from '@/pages/global/interfaces'
 
 import { ErrorResponse } from '../_utils/MySaveError'
+import { getAllState } from '../_utils/kv'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<AppState | ErrorResponse>
+  res: NextApiResponse<Record<string, unknown> | null | ErrorResponse>
 ) {
   if (req.method === HttpRequestMethods.GET) {
-    res.status(200).json(global.state)
+    const state = await getAllState()
+    res.status(200).json(state)
   } else {
     res.status(501).json({ error: 'Unsupported request' })
   }
