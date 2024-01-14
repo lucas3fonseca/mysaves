b76108b51b1863e0946f96a314e3b178c12589bf
Lucas Fonseca <lfonseca@efuse.io>
2024-01-14 14:49:27 -0500

    setup css grid for previews

--- src/home/components/HomeView.tsx
+++ src/home/components/HomeView.tsx
@@ -3,21 +3,22 @@ import { MySavePreviewContainer } from './preview/MySavePreviewContainer'
 
 export const HomeView = () => {
   return (
-    <div className='flex space-x-4 flex-wrap'>
+    <>
       <div className='
-        mt-5 ml-5 bg-gradient-to-b from-mysave-cyan
+        mt-5 bg-gradient-to-b from-mysave-cyan
         border-transparent rounded-xl
-        w-1/2 bg-no-repeat bg-contain
+        bg-no-repeat bg-contain
     '>
-        <div className='flex flex-col justify-center flex-wrap min-h-72'>
+        <div className='flex flex-col justify-center flex-wrap w-full min-h-52'>
           <p className='m-auto p-5 text-2xl text-wrap text-center font-semibold'>
             Save all your favorite YouTube videos in one place!
           </p>
           <CreateContainer />
         </div>
       </div>
-
-      <MySavePreviewContainer />
-    </div>
+      <div className='grid md:grid-cols-4 sm:grid-cols-3 gap-4 m-3'>
+        <MySavePreviewContainer />
+      </div>
+    </>
   )
 }
--- src/home/components/preview/MySavePreviewView.tsx
+++ src/home/components/preview/MySavePreviewView.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import type { CloudinaryImage, MySave } from '@/pages/global/interfaces'
+import type { MySave } from '@/pages/global/interfaces'
 
 interface MySavePreviewViewProps {
   mySaves: MySave[]
@@ -14,25 +14,30 @@ export const MySavePreviewView = ({ mySaves }: MySavePreviewViewProps) => {
 
         mySaves.map(({ cloudinaryThumbnail: { publicId, format, blurDataUrl }, id }) => {
           return (
-            <Link
-              key={publicId}
-              href={`/my-save/${id}`}
-              shallow
-            >
-              <Image
-                alt='thumbnail'
-                className='border-transparent rounded-xl'
-                placeholder='blur'
-                blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${publicId}.${format}`}
-                width={520}
-                height={380}
-                sizes='(max-width: 640px) 100vw,
-                (max-width: 1280px) 50vw,
-                (max-width: 1536px) 33vw,
-                25vw'
-              />
-            </Link>
+            <div key={publicId}
+              className='border rounded-xl mt-5
+              hover:border-mysave-pink
+            '>
+              <Link
+                key={publicId}
+                href={`/my-save/${id}`}
+                shallow
+              >
+                <Image
+                  alt='thumbnail'
+                  className='border-transparent rounded-xl w-full'
+                  placeholder='blur'
+                  blurDataURL={blurDataUrl}
+                  src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${publicId}.${format}`}
+                  width={720}
+                  height={480}
+                  sizes='(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw'
+                />
+              </Link>
+            </div>
           )
         })
       }
